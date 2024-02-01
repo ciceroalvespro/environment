83aa4a9a880d45416236121a71bbbe79906ac6d9
ciceroalvespro <143018315+ciceroalvespro@users.noreply.github.com>
2024-02-01 13:57:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+import plotly.express as px
 import streamlit as st
+import pandas as pd
+
+# Dados de exemplo
+data = {
+    'Categoria': ['A', 'B', 'C', 'D'],
+    'Valores': [30, 20, 40, 10]
+}
+
+df = pd.DataFrame(data)
 
 # Título do Dashboard
 #st.title('Meu Dashboard Básico')
@@ -28,12 +38,21 @@ st.markdown("## Gráficos")
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.header('Gráfico 1')
-    # Adicione o código para o primeiro gráfico aqui
+    st.header('gráfico de pizza')
+    # Criando o gráfico de pizza
+    fig = px.pie(df, values='Valores', names='Categoria', title='Gráfico de Pizza')
+
+    # Mostrando o gráfico no Streamlit
+    st.plotly_chart(fig, use_container_width=True)
 
 with col2:
-    st.header('Gráfico 2')
+    st.header('gráfico de barras')
     # Adicione o código para o segundo gráfico aqui
+    # Criando o gráfico de barras
+    fig = px.bar(df, x='Categoria', y='Valores', title='Gráfico de Barras')
+
+# Mostrando o gráfico no Streamlit
+st.plotly_chart(fig, use_container_width=True)
 
 with col3:
     st.header('Gráfico 3')
